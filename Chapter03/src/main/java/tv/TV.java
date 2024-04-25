3e2600f007aed0f28f2ea869d989da0f8ce861df
cksghks89 <song960705@naver.com>
2024-04-25 16:15:34 +0900

    chapter03 watchTV 수정

--- Chapter03/src/main/java/tv/TV.java
+++ Chapter03/src/main/java/tv/TV.java
@@ -2,13 +2,13 @@ package tv;
 
 public class TV {
 	private int channel;	// 1 ~ 255
-	private int volume;		// 0 ~ 10
+	private int volume;		// 0 ~ 100
 	private boolean power;
 
 	public TV(int channel, int volume, boolean power) {
-		this.channel = channel;
-		this.volume = volume;
-		this.power = power;
+		this.channel(channel);
+		this.volume(volume);
+		this.power = power;		
 	}
 	
 	public void power(boolean on) {
@@ -16,19 +16,21 @@ public class TV {
 	}
 	
 	public void channel(int channel) {
-		this.channel = channel;
+		this.channel = (channel - 1) % 255 + 1;
 	}
 	
 	public void channel(boolean up) {
 		this.channel += up ? 1 : -1;
+		this.channel(this.channel);
 	}
 	
 	public void volume(int volume) {
-		this.volume = volume;
+		this.volume = volume % 101;
 	}
 	
 	public void volume(boolean up) {
 		this.volume += up ? 1 : -1;
+		this.volume(this.volume);
 	}
 	
 	public void status() {
