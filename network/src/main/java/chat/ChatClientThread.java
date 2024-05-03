92e779ff9bb1fe53aa323821788696977bfbf004
cksghks89 <song960705@naver.com>
2024-05-03 11:12:09 +0900

    socket chatting program - chat client join method modified

--- network/src/main/java/chat/ChatClientThread.java
+++ network/src/main/java/chat/ChatClientThread.java
@@ -31,9 +31,9 @@ public class ChatClientThread extends Thread {
 					continue;
 				}
 
-				if ("message".equals(tokens[0])) {
+				if ("message".equals(tokens[0]) && tokens.length >= 2) {
 					System.out.println(tokens[1]);
-				} else if ("join".equals(tokens[0])) {
+				} else if ("join:ok".equals(response)) {
 					System.out.println("입장하였습니다. 즐거운 채팅 되세요~!");
 				}
 			}
